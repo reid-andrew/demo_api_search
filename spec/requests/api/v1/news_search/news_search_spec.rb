bbc2a5493867702d968124e94ffb618ff539e5c3
Andrew Reid <areid.sj@gmail.com>
2020-09-02 14:32:26 -0600

    Refactor test spec for readability

--- spec/requests/api/v1/news_search/news_search_spec.rb
+++ spec/requests/api/v1/news_search/news_search_spec.rb
@@ -11,66 +11,70 @@ RSpec.describe 'News Search - ', type: :request do
                 .to_return(status: 200, body: baseball_term_response, headers: {})
   end
 
-  it 'finds news stories with no search term' do
-    get '/api/v1/news_search'
+  describe 'test searching for stories - ' do
+    it 'finds news stories with no search term' do
+      get '/api/v1/news_search'
 
-    output = JSON.parse(response.body, symbolize_names: true)
+      output = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
-    expect(output[:data][:attributes][:stories].count).to eq(10)
-    expect(output[:data][:id]).to eq(Search.last.id.to_s)
-    expect(output[:data][:attributes][:stories][0][:web_url]).to eq("https://www.nytimes.com/reuters/2020/09/02/world/europe/02reuters-health-coronavirus-greece-russia.html")
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(output[:data][:attributes][:stories].count).to eq(10)
+      expect(output[:data][:id]).to eq(Search.last.id.to_s)
+      expect(output[:data][:attributes][:stories][0][:web_url]).to eq("https://www.nytimes.com/reuters/2020/09/02/world/europe/02reuters-health-coronavirus-greece-russia.html")
+    end
+
+    it 'finds news stories with a search term' do
+      get '/api/v1/news_search?search=baseball'
+
+      output = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(output[:data][:attributes][:stories].count).to eq(10)
+      expect(output[:data][:id]).to eq(Search.last.id.to_s)
+      expect(output[:data][:attributes][:stories][0][:web_url]).to eq("https://www.nytimes.com/2020/08/28/sports/baseball/mlb-protest-canceled-games.html")
+      expect(output[:data][:attributes][:stories][1][:abstract]).to eq("The Dodgers are 22-8 after 30 games of a 60-game season that will be remembered for postponed games, health-driven rules and expanded playoffs.")
+    end
   end
 
-  it 'finds news stories with a search term' do
-    get '/api/v1/news_search?search=baseball'
+  describe 'test sorting stories - ' do
+    it 'sorts stories found without a search term in ascending order' do
+      get '/api/v1/news_search?sort=asc'
 
-    output = JSON.parse(response.body, symbolize_names: true)
+      output = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
-    expect(output[:data][:attributes][:stories].count).to eq(10)
-    expect(output[:data][:id]).to eq(Search.last.id.to_s)
-    expect(output[:data][:attributes][:stories][0][:web_url]).to eq("https://www.nytimes.com/2020/08/28/sports/baseball/mlb-protest-canceled-games.html")
-    expect(output[:data][:attributes][:stories][1][:abstract]).to eq("The Dodgers are 22-8 after 30 games of a 60-game season that will be remembered for postponed games, health-driven rules and expanded playoffs.")
-  end
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(output[:data][:attributes][:stories].count).to eq(10)
+      expect(output[:data][:id]).to eq(Search.last.id.to_s)
+      expect(output[:data][:attributes][:stories][0][:abstract]).to eq("Authorities in Madrid shut down a coronavirus testing center for teachers on Wednesday after large crowds formed outside, while the health ministry diagnosed 3,663 new cases of the virus, down from a peak of nearly 10,000 recorded in Spain last Friday. ")
+      expect(output[:data][:attributes][:stories][1][:web_url]).to eq("https://www.nytimes.com/reuters/2020/09/02/world/europe/02reuters-russia-politics-nalvany-britain-johnson.html")
+    end
 
-  it 'sorts stories found without a search term in ascending order' do
-    get '/api/v1/news_search?sort=asc'
+    it 'sorts stories found with a search term in descending order' do
+      get '/api/v1/news_search?sort=desc&search=baseball'
 
-    output = JSON.parse(response.body, symbolize_names: true)
+      output = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
-    expect(output[:data][:attributes][:stories].count).to eq(10)
-    expect(output[:data][:id]).to eq(Search.last.id.to_s)
-    expect(output[:data][:attributes][:stories][0][:abstract]).to eq("Authorities in Madrid shut down a coronavirus testing center for teachers on Wednesday after large crowds formed outside, while the health ministry diagnosed 3,663 new cases of the virus, down from a peak of nearly 10,000 recorded in Spain last Friday. ")
-    expect(output[:data][:attributes][:stories][1][:web_url]).to eq("https://www.nytimes.com/reuters/2020/09/02/world/europe/02reuters-russia-politics-nalvany-britain-johnson.html")
-  end
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(output[:data][:attributes][:stories].count).to eq(10)
+      expect(output[:data][:id]).to eq(Search.last.id.to_s)
+      expect(output[:data][:attributes][:stories][0][:abstract]).to eq("There’s no denying his charm and genuine passion for baseball. But his habit of overstepping boundaries would have made him an uncomfortable presence if he had succeeded in purchasing the Mets.")
+      expect(output[:data][:attributes][:stories][1][:web_url]).to eq("https://www.nytimes.com/2020/08/21/sports/baseball/coronavirus-mets-yankees.html")
+    end
 
-  it 'sorts stories found with a search term in descending order' do
-    get '/api/v1/news_search?sort=desc&search=baseball'
+    it 'returns stories unsorted if unacceptable sort term provided' do
+      get '/api/v1/news_search?sort=fail'
 
-    output = JSON.parse(response.body, symbolize_names: true)
+      output = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
-    expect(output[:data][:attributes][:stories].count).to eq(10)
-    expect(output[:data][:id]).to eq(Search.last.id.to_s)
-    expect(output[:data][:attributes][:stories][0][:abstract]).to eq("There’s no denying his charm and genuine passion for baseball. But his habit of overstepping boundaries would have made him an uncomfortable presence if he had succeeded in purchasing the Mets.")
-    expect(output[:data][:attributes][:stories][1][:web_url]).to eq("https://www.nytimes.com/2020/08/21/sports/baseball/coronavirus-mets-yankees.html")
-  end
-
-  it 'returns stories unsorted if unacceptable sort term provided' do
-    get '/api/v1/news_search?sort=fail'
-
-    output = JSON.parse(response.body, symbolize_names: true)
-
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
-    expect(output[:data][:attributes][:stories].count).to eq(10)
-    expect(output[:data][:id]).to eq(Search.last.id.to_s)
-    expect(output[:data][:attributes][:stories][0][:web_url]).to eq("https://www.nytimes.com/reuters/2020/09/02/world/europe/02reuters-health-coronavirus-greece-russia.html")
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(output[:data][:attributes][:stories].count).to eq(10)
+      expect(output[:data][:id]).to eq(Search.last.id.to_s)
+      expect(output[:data][:attributes][:stories][0][:web_url]).to eq("https://www.nytimes.com/reuters/2020/09/02/world/europe/02reuters-health-coronavirus-greece-russia.html")
+    end
   end
 end
