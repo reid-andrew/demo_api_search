0815faa87a79060ea1d8e295adf29e991c6a8f67
Andrew Reid <areid.sj@gmail.com>
2020-09-02 11:40:00 -0600

    Update search table and model to remove date column

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,3 @@
 class Search < ApplicationRecord
-  validates :date, presence: true
   validates :search, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_172035) do
+ActiveRecord::Schema.define(version: 2020_09_02_173838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "searches", force: :cascade do |t|
-    t.datetime "date"
     t.string "search"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
